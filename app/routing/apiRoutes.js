4ffae71a433d4e7451760e322f75a38d980e89e2
Kareem Lofters <kareem.lofters@gmail.com>
2018-09-15 17:58:47 -0400

    completed home and survey html, completed get method in apiRoutes, htmlRoutes complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,3 +5,19 @@
 // ===============================================================================
 
 var friendData = require('../data/friends');
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get('/api/friends', function(req, res) {
+    res.json(friendData);
+  });
+};
