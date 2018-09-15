d444605866fc0a45166b3ceffd06711ec74dee0b
Kareem Lofters <kareem.lofters@gmail.com>
2018-09-15 17:21:05 -0400

    added file structure and folders

--- server.js
+++ server.js
@@ -12,3 +12,9 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log('App listening on PORT ' + PORT);
+});
