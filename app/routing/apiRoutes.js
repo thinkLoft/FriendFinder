ca9b86da30b298a9ebe0de2d85de3e0e1928ce42
Kareem Lofters <kareem.lofters@gmail.com>
2018-09-15 17:40:14 -0400

    completed htmlRoutes, populate home.html with dummy data, populated friends.js with dummy data

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,7 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendData = require('../data/friends');
