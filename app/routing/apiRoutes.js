41fe1ac81505b43643096b8d03c28a5ea9f4a7b7
Kareem Lofters <kareem.lofters@gmail.com>
2018-09-15 19:40:59 -0400

    added logic to check the difference between friends, first draft complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,4 +20,48 @@ module.exports = function(app) {
   app.get('/api/friends', function(req, res) {
     res.json(friendData);
   });
+
+  app.post('/api/friends', function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+
+    // Variable to save closest match
+    var closestMatch = {
+      friend: undefined,
+      difference: undefined
+    };
+
+    // grab User Input
+    var userScore = req.body.scores;
+    var difference;
+
+    // convert User Input to integer
+    for (i = 0; i < userScore.length; i++) {
+      userScore[i] = parseInt(userScore[i].trim(), 10);
+    }
+
+    // Loop to compare user Input with each Friend
+    for (i = 0; i < friendData.length; i++) {
+      comparedUser = friendData[i].scores;
+      difference = 0;
+
+      // loop to compare scores
+      for (x = 0; x < userScore.length; x++) {
+        difference += Math.abs(userScore[x] - comparedUser[x]);
+      }
+
+      // Logic to check the difference
+      if (
+        difference < closestMatch.difference ||
+        closestMatch.difference === undefined
+      ) {
+        closestMatch.friend = friendData[i];
+        closestMatch.difference = difference;
+        console.log(closestMatch);
+      }
+    }
+
+    res.json(closestMatch.friend);
+  });
 };
